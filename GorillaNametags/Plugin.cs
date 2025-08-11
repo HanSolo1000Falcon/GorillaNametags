a4d8dbbc84813d1aa563eefe3f3d285b007b3b77
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2025-08-11 17:29:15 +0200

    asdfasdf

--- GorillaNametags/Plugin.cs
+++ GorillaNametags/Plugin.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using BepInEx;
+using ExitGames.Client.Photon;
 using GorillaNametags.Components;
 using GorillaNametags.Patches;
 using GorillaNametags.Tags;
+using Photon.Pun;
 using TMPro;
 using UnityEngine;
 
@@ -16,30 +18,34 @@ public class Plugin : BaseUnityPlugin
 {
     public static Transform firstPersonCamera;
     public static Transform thirdPersonCamera;
-    
+
     public static TMP_FontAsset comicSans;
-    
+
     public const string FirstPersonLayerName = "FirstPersonOnly";
     public const string ThirdPersonLayerName = "MirrorOnly";
-    
+
     public static Dictionary<VRRig, UserIDTag> userIDTags = new();
     public static Dictionary<string, DateTime> createdDates = new();
-    
+
     private void Start()
     {
         HarmonyPatches.ApplyHarmonyPatches();
         GorillaTagger.OnPlayerSpawned(OnGameInitialized);
+
+        PhotonNetwork.LocalPlayer.SetCustomProperties(new Hashtable()
+            { { "GorillaNametags", "Made by HanSolo1000Falcon B)" } });
     }
 
     private void OnGameInitialized()
     {
         firstPersonCamera = GorillaTagger.Instance.mainCamera.transform;
         thirdPersonCamera = GorillaTagger.Instance.thirdPersonCamera.transform.GetChild(0);
-        
-        Stream bundleStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("GorillaNametags.Resources.gorillanametags");
+
+        Stream bundleStream = Assembly.GetExecutingAssembly()
+            .GetManifestResourceStream("GorillaNametags.Resources.gorillanametags");
         AssetBundle bundle = AssetBundle.LoadFromStream(bundleStream);
         bundleStream.Close();
-        
+
         comicSans = bundle.LoadAsset<TMP_FontAsset>("COMICBD SDF");
         comicSans.material.shader = Shader.Find("TextMeshPro/Distance Field");
 
@@ -52,7 +58,7 @@ public class Plugin : BaseUnityPlugin
         tagObject.transform.SetParent(parent);
         tagObject.transform.localPosition = localPosition;
         tagObject.layer = LayerMask.NameToLayer(layerName);
-        
+
         TextMeshPro tagText = tagObject.AddComponent<TextMeshPro>();
         tagText.font = comicSans;
         tagText.fontSize = 1;
